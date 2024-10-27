86482d821d64a5735dc9a4b3967029faa2e0a286
panoslin <lghpanos@gmail.com>
2024-10-27 00:12:22 -0400

    add: kernel function till reduce to shared memory

--- knn_cuda.cu
+++ knn_cuda.cu
@@ -16,19 +16,106 @@
 using namespace std;
 
 
-__global__ void find_knn(
+__global__ void find_d_knn(
     float* d_train_matrix,
-    float *d_test_matrix, 
-    int* d_predicitons, 
-    thrust::pair<int, float> *d_knn,
-    int k
-) {
+    float* d_test_matrix,
+    int* d_predicitons,
+    thrust::pair<float, float>* d_knn,
+    int k,
+    int blockPerTestInstance,
+    int trainInstancePerThread,
+    int trainInstancePerBlock,
+    int train_num_instances,
+    int num_attributes,
+    int num_classes,
+    int threadPerBlock
+)
+{
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // 1. Find the test instance index
+    int testInstanceIdx = blockIdx.x / blockPerTestInstance;
+
+    // 2. Find the starting point of train instances for the current thread
+    // find the idx of the current block for the current test instance
+    // TODO: memory access pattern having strides
+    int blockIdxForTestInstance = blockIdx.x % blockPerTestInstance;
+    int trainInstanceIdx = blockIdxForTestInstance * trainInstancePerBlock + threadIdx.x * trainInstancePerThread;
+
+    // 3. Calculate the #trainInstancePerThread consequtive train instances dist and store them thread locally
+    float threadLocalKnn[2 * k];
+    for (int i = 0; i < 2 * k; i++) { 
+        threadLocalKnn[i] = FLT_MAX; 
+    }
+
+    for (int i = trainInstanceIdx; i < trainInstanceIdx + trainInstancePerThread && i < train_num_instances; i++) {
+        // 1. calculate distance of the pair
+        float dist = 0.0f;
+        for (int attr_i = 0; attr_i < num_attributes - 1; ++attr_i) {
+            float diff = d_train_matrix[i * num_attributes + attr_i] - d_test_matrix[testInstanceIdx * num_attributes + attr_i];
+            dist += diff * diff;
+        }
+        // 2. wirte to threadLocalKnn
+        for (int c = 0; c < k; c++) {
+            if (dist < threadLocalKnn[2 * c]) {
+                // Found a new candidate
+                // Shift previous candidates down by one
+                for (int x = k - 1; x > c; x--) {
+                    threadLocalKnn[2 * x] = threadLocalKnn[2 * (x - 1)];
+                    threadLocalKnn[2 * x + 1] = threadLocalKnn[2 * (x - 1) + 1];
+                }
+
+                // Set key vector as potential k NN
+                threadLocalKnn[2 * c] = dist;
+                threadLocalKnn[2 * c + 1] = d_train_matrix[i * num_attributes + num_attributes - 1];
+                break;
+            }
+        }
+    }
+
+    // 4. Find the block knn
+    // store the knn for each thread
+    __shared__ thrust::pair<float, float> blockLocalKnnCandidates[threadPerBlock * k];
+    // find the thread local knn and write to shared memory blockLocalKnnCandidates
+    for (int i = 0; i < k; i++) {
+        int sharedMemIndex = threadIdx.x * k + i;
+        if (threadLocalKnn[i * 2] != FLT_MAX) {
+            blockLocalKnnCandidates[sharedMemIndex] = thrust::make_pair(
+                threadLocalKnn[i * 2], 
+                threadLocalKnn[i * 2 + 1]
+            );
+        }
+    }
+
+    // make sure all threads in the block have completed
+    __syncthreads();
+
+    // 5. Find the knn from the blockLocalKnnCandidates and write to d_knn
+    // each thread divisible by 96 reduce the previous 96 or less instances to knn
 
 }
 
+__global__ void find_knn(
+    float* d_train_matrix,
+    float* d_test_matrix,
+    int* d_predicitons,
+    thrust::pair<float, float>* d_knn,
+    int k,
+    int blockPerTestInstance,
+    int trainInstancePerThread,
+    int trainInstancePerBlock,
+    int train_num_instances
+)
+{
+
+    // TODO: this should be the second kernel, so to gurantee the d_knn are synchronized
+    // 6. find the majority if for each block knn
+    // 7. write to d_predicitons
+}
 
 // Calculates the distance between two instances
-float distance(const float* instance_A, const float* instance_B, int num_attributes) {
+__device__ float distance(const float* instance_A, const float* instance_B, int num_attributes)
+{
     float sum = 0.0f;
     for (int i = 0; i < num_attributes - 1; ++i) { // Exclude the class label
         float diff = instance_A[i] - instance_B[i];
@@ -45,12 +132,14 @@ struct Candidate {
 
 // Comparator for the priority queue (max-heap)
 struct CandidateComparator {
-    bool operator()(const Candidate& lhs, const Candidate& rhs) const {
+    bool operator()(const Candidate& lhs, const Candidate& rhs) const
+    {
         return lhs.distance < rhs.distance;
     }
 };
 
-vector<int> KNN(ArffData* train, ArffData* test, int k) {
+vector<int> KNN(ArffData* train, ArffData* test, int k)
+{
     int num_classes = train->num_classes();
     int num_attributes = train->num_attributes();
     int train_num_instances = train->num_instances();
@@ -72,7 +161,7 @@ vector<int> KNN(ArffData* train, ArffData* test, int k) {
     int trainInstancePerThread = trainInstancePerBlock / threadPerBlock;
 
     // 1. init mem
-    float* d_train_matrix, *d_test_matrix;
+    float* d_train_matrix, * d_test_matrix;
 
     cudaMalloc(&d_train_matrix, sizeof(float) * num_attributes * train_num_instances);
     cudaMalloc(&d_test_matrix, sizeof(float) * num_attributes * test_num_instances);
@@ -82,22 +171,47 @@ vector<int> KNN(ArffData* train, ArffData* test, int k) {
 
     // store the knns for each local knn for each block for each test instance
     // each block will calculate the knn locally and write to this d_knn global memory
-    thrust::pair<int, float> *d_knn;
+    thrust::pair<float, float>* d_knn;
     cudaMalloc((void**)&d_knn, test_num_instances * blockPerTestInstance * k * sizeof(thrust::pair<int, float>));
 
     // 2. Copy to device
     cudaMemcpy(d_train_matrix, train_matrix, sizeof(float) * num_attributes * train_num_instances, cudaMemcpyHostToDevice);
     cudaMemcpy(d_test_matrix, test_matrix, sizeof(float) * num_attributes * test_num_instances, cudaMemcpyHostToDevice);
-    
+
     // Set init value to d_predicitons
     cudaMemset(d_predictions, 0, sizeof(int) * test_num_instances);
 
     // 4. Call kernel
-    find_knn<<< blockPerGrid, threadPerBlock>>> (d_train_matrix, d_test_matrix, d_predictions, d_knn, k);
+    find_d_knn << < blockPerGrid, threadPerBlock >> > (
+        d_train_matrix,
+        d_test_matrix,
+        d_predictions,
+        d_knn,
+        k,
+        blockPerTestInstance,
+        trainInstancePerThread,
+        trainInstancePerBlock,
+        train_num_instances,
+        num_attributes,
+        num_classes,
+        threadPerBlock
+    );
+
+    find_knn << < blockPerGrid, threadPerBlock >> > (
+        d_train_matrix,
+        d_test_matrix,
+        d_predictions,
+        d_knn,
+        k,
+        blockPerTestInstance,
+        trainInstancePerThread,
+        trainInstancePerBlock,
+        train_num_instances
+        );
 
     // 5. Copy to host
     cudaMemcpy(predictions.data(), d_predictions, sizeof(int) * test_num_instances, cudaMemcpyDeviceToHost);
-    
+
     // 6. Free memory
     cudaFree(d_train_matrix);
     cudaFree(d_test_matrix);
@@ -107,7 +221,8 @@ vector<int> KNN(ArffData* train, ArffData* test, int k) {
     return predictions;
 }
 
-vector<int> computeConfusionMatrix(const vector<int>& predictions, ArffData* dataset) {
+vector<int> computeConfusionMatrix(const vector<int>& predictions, ArffData* dataset)
+{
     int num_classes = dataset->num_classes();
     int num_instances = dataset->num_instances();
     vector<int> confusionMatrix(num_classes * num_classes, 0);
@@ -122,7 +237,8 @@ vector<int> computeConfusionMatrix(const vector<int>& predictions, ArffData* dat
     return confusionMatrix;
 }
 
-float computeAccuracy(const vector<int>& confusionMatrix, ArffData* dataset) {
+float computeAccuracy(const vector<int>& confusionMatrix, ArffData* dataset)
+{
     int num_classes = dataset->num_classes();
     int successfulPredictions = 0;
 
@@ -133,7 +249,8 @@ float computeAccuracy(const vector<int>& confusionMatrix, ArffData* dataset) {
     return 100.0f * successfulPredictions / dataset->num_instances();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     if (argc != 4) {
         cerr << "Usage: ./program datasets/train.arff datasets/test.arff k" << endl;
         return 1;
@@ -164,10 +281,10 @@ int main(int argc, char* argv[]) {
 
 
     cout << "The " << k << "-NN classifier for " << test->num_instances()
-         << " test instances and " << train->num_instances()
-         << " train instances required " << milliseconds
-         << " ms CPU time for single-thread. Accuracy was "
-         << accuracy << "%" << endl;
+        << " test instances and " << train->num_instances()
+        << " train instances required " << milliseconds
+        << " ms CPU time for single-thread. Accuracy was "
+        << accuracy << "%" << endl;
 
     return 0;
 }
